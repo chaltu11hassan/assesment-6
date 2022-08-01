4efd26e44bec1a2eae4a857c2781855a42670f61
Chaltu Hassan <chaltu11hassan@gmail.com>
2022-08-01 09:55:31 -0500

    initial commit

---
+++ duelDuo.test.ts
@@ -0,0 +1,34 @@
+
+import { Builder, Capabilities, By } from "selenium-webdriver"
+
+require('chromedriver')
+
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+
+beforeEach(async () => {
+    driver.get('http://localhost:3000/')
+})
+
+afterAll(async () => {
+    driver.quit()
+})
+
+test('Title shows up when page loads', async () => {
+    const title = await driver.findElement(By.id('title'))
+    const displayed = await title.isDisplayed()
+    expect(displayed).toBe(true)
+})
+
+test('add to duo button should work giving div id',async ()=> {
+    await driver.findElement(By. id('draw')).click();
+
+    await driver.sleep(2000);
+
+    await driver.findElement(By.css('.bot-btn')).click();
+
+    await driver.sleep(2000);
+
+    const duoDiv = await driver.findElement(By.id('player-duo'));
+
+    expect(duoDiv.isDisplayed()).toBeTruthy();
+})
---
+++ heroku.md
@@ -0,0 +1,6 @@
+## Paste your link below
+
+
+
+
+https://trello.com/invite/b/jkeTbl41/49143e58fbca11e64d4164877dce6c59/assesment-6
